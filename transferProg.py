acebc6b1e33e93c76f8190aa5ef4071d07aa725b
libra guest <stevo81989@gmail.com>
2012-07-01 11:26:50 -0400

    transferProg.py

--- transferProg.py
+++ transferProg.py
@@ -1,11 +1,7 @@
 import ftplib
 import os
 import sys
-try:	
-	import paramiko
-except:
-	print("Paramiko is not installed. Visit lag.net/paramiko")
-	
+
 def ftpup(host,username,password,directory,file):
 	os.chdir(directory)
 	s = ftplib.FTP(host,username,password) # Connect
@@ -17,6 +13,10 @@ def ftpup(host,username,password,directory,file):
 
 # PATH VARIABLE MUST INCLUDE WHAT THE FILENAME WILL BE ON THE SERVER
 def sftpup(host,username,password,file,path):
+    try:    
+	    import paramiko
+    except:
+	    print("Paramiko is not installed. Visit lag.net/paramiko")
 	ssh = paramiko.SSHClient() 
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
